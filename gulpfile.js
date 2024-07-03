fb476da6fe8d97bc0a41ba4a94dfcd306c27b2b3
Jeferson Soares <jpadilha53@gmail.com>
2024-07-03 11:54:20 -0300

    criando o hero e importanto a fonte avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
 
 function styles() {
   return gulp
@@ -12,7 +13,14 @@ function styles() {
     .pipe(gulp.dest("./dist/css"));
 }
 
-exports.default = styles;
+function images() {
+  return gulp
+    .src("./src/images/*/**")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/images/"));
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function () {
   gulp.watch("./src/styles/*.sass", gulp.parallel(styles));
 };
